4e1a439f03835ef13e7ec25921b0eb3d06ec747a
MartinGacev <martin.gacev@gmail.com>
2024-12-09 14:42:47 +0100

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let phone = 30
+let priceOfPhone = 199.95
+let tax = 5 
+let fullTax = 100
+resultOfFiveTax = (phone * priceOfPhone) / fullTax * tax
+console.log(resultOfFiveTax)
+
+
+
+let minute = 20
+let seconds = 60
+resultOfMinutesToSeconds = minute * seconds 
+console.log(resultOfMinutesToSeconds)
+
+
+let number = 10
+result = number + 1
+console.log(result)
+
+
+let base = 15
+let height = 10
+resultOfBaseHeight = (base * height) / 2
+console.log(resultOfBaseHeight)
